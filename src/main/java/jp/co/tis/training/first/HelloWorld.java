bac50cc434d49bc95dacd274be6316e795ddd6c0
unknown <tie304681@tisnt.tis.co.jp>
2016-07-08 11:50:41 +0900

    add ?

--- src/main/java/jp/co/tis/training/first/HelloWorld.java
+++ src/main/java/jp/co/tis/training/first/HelloWorld.java
@@ -3,6 +3,6 @@ package jp.co.tis.training.first;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		System.out.println("Hello World?????????????.");
 	}
 }
